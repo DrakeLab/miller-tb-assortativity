dfa2ac9aaddd62feab351f44a309b50d9cb42dbb
Paige Miller <pbmpb13@uga.edu>
2019-12-17 14:24:07 -0500

    try catch to avoid errors

---
+++ analysis/simulations-sah/make_nets_try_catch.py
@@ -0,0 +1,42 @@
+### Import modules
+
+import networkx as nx
+import random_modular_generator_variable_modules as rmg
+import sequence_generator as sg
+from sklearn.model_selection import ParameterGrid
+import numpy as np
+import os
+import csv
+import random
+
+os.chdir('/Users/paigemiller/Documents/UGA/phd/research-projects/miller-tb-assortativity/analysis/simulations-sah')
+
+##### Parameters for SIR simulations
+# Network params
+N= [1000]                                  # network sizes
+Q= [0, 0.15, 0.3, 0.45]                    # network modularity 
+d= 10                                      # mean degree
+m= 2                                       # number of modules (sexes)
+sfunction = sg.geometric_sequence          # degree distribution 
+modfunction = sg.regular_sequence          # module size distribution
+
+var_grid = list(ParameterGrid({'N' : N, 'Q' : Q}))
+
+# Loop params
+reps= 5
+
+##### Create and save networks  ########
+
+try: 
+	for x in range(0, len(var_grid)): 
+	    for y in range(0, reps):
+	        n=var_grid[x]["N"]
+	        q=var_grid[x]["Q"]
+	    
+	        # generating network
+	        G = rmg.generate_modular_networks(n, sfunction, modfunction, q, m, d)
+	        nx.write_graphml(G, "networks/GG_Q"+str(q)+"_N"+str(n)+"_rep"+str(y)+".graphml")
+except Exception as ex: 
+	pass
+
+print('This somehow worked!')    
