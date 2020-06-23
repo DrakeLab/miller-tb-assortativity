a9745aa8771e839bcf7c7920dfef1802670b6b23
Paige Miller <pbmpb13@uga.edu>
2020-06-23 11:41:28 -0400

    fixing

--- analysis/simulations/slirs_par_debug_sah.py
+++ analysis/simulations/slirs_par_debug_sah.py
@@ -42,8 +42,8 @@ def process_file(f):
     H.add_edge('R.m', 'S.m', rate = psi)   # male
 
     #S->I spontaneous infection
-    H.add_edge('S.f', 'I.f', rate = psi/50)   # female
-    H.add_edge('S.m', 'I.m', rate = psi/50)   # male
+    H.add_edge('S.f', 'I.f', rate = psi/50.)   # female
+    H.add_edge('S.m', 'I.m', rate = psi/50.)   # male
 
     # INDUCED transitions
     J = nx.DiGraph()
@@ -148,8 +148,8 @@ def process_file(f):
 
 ## Model parameters ##
 N = [1000]           # Network Size
-R = [0, .05, .1,.15, .2,.25, .3,.35] # Actually Q (Newman)
-Tau = [0.04, 0.075, 0.1, 0.15]         #  S->L Baseline transmission rate 
+R = [0, 0.1, 0.2, 0.3] # Actually Q (Newman)
+Tau = [0.04] #[0.04, 0.075, 0.1, 0.15]         #  S->L Baseline transmission rate 
 Del = [100000, 1./10.]     # L->I Reactivation rate; 10000=>SIR, del~0=SLIR
 Gam = 1./2.          # I->R Recovery rate
 Psi =  [0, 0.33]       # R->S Reversion rate; 0=SIR, sig>0=SIRS
@@ -157,13 +157,13 @@ i0 = 0.05            # proportion initially infected
 tsteps = 250         # set max time steps to run model for
 
 # Male:female differences to explain male bias
-Alph_vals = [1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0]   # Ratio of male:female susceptibility
+Alph_vals =  [3.0] #[1.0, 1.25, 1.5, 2.0, 3.0]   # Ratio of male:female susceptibility
 Alph_types = ["SUS", "TRA", "INF_PER"]
 
 # Network parameters
 nt = ["SAH"]
 
-reps = range(1,300) # Number of reps 
+reps = range(0,1) # Number of reps 
 
 var_grid = list(ParameterGrid({'N' : N, 'R' : R, 'Tau': Tau,
                                'Psi' : Psi, 'Del' : Del,
@@ -177,7 +177,7 @@ sim_results = p.map(process_file, var_grid) # perform the calculations
 
 #print(sim_results)
 
-with open("SLIRS-res/"+"sah_res_300.csv",'wb') as out:
+with open("SLIRS-res/"+"sah_res_2.csv",'wb') as out:
     csv_out=csv.writer(out)
     csv_out.writerow(["failed", "net_size", "r", "tau", "alph_val", "alph_type", "delt", "psi", "rep","type_net", "net_clus", "net_path_len", "net_deg_assort", "peak", "sim_dur", "m_rec", "f_rec", "m_inf","f_inf", "tot_lat"])
     csv_out.writerows(sim_results)
